c12830906d8e4926ea91c090056f79d8f7b66634
benjamin <benjaminkauffmann@hotmail.fr>
2019-09-30 13:45:20 +0200

    optimize 2

--- Code/src/DBManager.java
+++ Code/src/DBManager.java
@@ -25,37 +25,19 @@ public class DBManager {
 			return false;
 		}
 
-		if(args[0].contentEquals("create") && args[1] != null && isInteger(args[2])) {
-			int nbColumn = Integer.parseInt(args[2]);			
-			try {
-				RandomAccessFile file = new RandomAccessFile(args[1]+".txt", "rw");
-				
-				
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+		if(args[0].contentEquals("create")) {
+			this.CreateRelation(args[1], Integer.parseInt(args[2]), typeColumn);
 		}
 		
 		return true;
 	}
 	
 	public void CreateRelation(String name, int nbColumn, String [] typeColumn){
-		
+		try {
+			RandomAccessFile file = new RandomAccessFile(args[1]+".txt", "rw");
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public static boolean isInteger(String s) {
-	    return isInteger(s,10);
-	}
-
-	public static boolean isInteger(String s, int radix) {
-	    if(s.isEmpty()) return false;
-	    for(int i = 0; i < s.length(); i++) {
-	        if(i == 0 && s.charAt(i) == '-') {
-	            if(s.length() == 1) return false;
-	            else continue;
-	        }
-	        if(Character.digit(s.charAt(i),radix) < 0) return false;
-	    }
-	    return true;
-	}
-}
