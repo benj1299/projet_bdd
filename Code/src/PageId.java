c9d76d3773c1b3292d307b580474537f6d53e604
Sanaa Anton <ii11251@pc529-9.ens.math-info.univ-paris5.fr>
2019-10-07 14:36:50 +0200

    pageid methodes get

--- Code/src/PageId.java
+++ Code/src/PageId.java
@@ -1,13 +1,23 @@
 
 public class PageId {
-	/*
-	 * 
-	 * */
+	
 	private int fileIdx; 
 	private int pageIdx;
 	
+	/**
+	 * 
+	 * @param fileIdx
+	 * @param pageIdx
+	 */
 	public PageId(int fileIdx, int pageIdx ) {
 		this.fileIdx=fileIdx;
 		this.pageIdx=pageIdx;
+	}
+	public int getFileIdx() {
+		return this.fileIdx;
+	}
+	
+	public int getPageIdx() {
+		return this.pageIdx;
 	}
 }
