305150441004c915552f793130f6b55e509ae1e5
Benjamin Kauffmann <benjaminkauffmann@hotmail.fr>
2019-10-14 15:43:06 +0200

    Improved of BufferManager

--- Code/src/BufferManager.java
+++ Code/src/BufferManager.java
@@ -1,14 +1,15 @@
-
+import java.util.Vector;
 public class BufferManager {
 
 	private static BufferManager instance; 	
-	
+	private Vector<Frame> bufferPool = new Vector<Frame>();
+
 	public static BufferManager getInstance(){
 	    if(instance == null)
 	    	instance = new BufferManager();
 	    return instance;
 	}
-
+	
 	/**
 	 * Rempli (ou remplace si besoin) le buffer correspondant à la bonne frame 
 	 * avec le contenu de la page désignée par l’argument pageId
@@ -16,10 +17,25 @@ public class BufferManager {
 	 * @return un des buffers associés à une case
 	 */
 	public byte[] getPage(PageId pageId) {
+		// Verifie si la page est dans le bufferpool (bufferPool.get(i))
+		// Si c'est le cas, renvoyer la page
+		// Sinon verifier sur disk si la page y est (readPage)
+		// Si elle n'y est pas, renvoies une exception
+		
 		byte[] content = null;
-		for(int i = 0; bufferPull.length; i++) {
-			
+		
+		for(int i = 0; i < bufferPool.size(); i++) {
+			if(bufferPool.get(i).getPageId() == pageId)
+				bufferPool.get(i).increment();
+				return bufferPool.get(i).getBuff();
 		}
+		
+		for(Frame x : this.bufferPool) {
+			if(x.isNull()) {
+				// rajouter pageId dans le bufferPool au premier emplacement initialisé (null)
+			}
+		}
+		
 		return content;
 	}
 	
@@ -28,8 +44,13 @@ public class BufferManager {
 	 * @param pageId
 	 * @param valdirty
 	 */
-	public void freePage(PageId pageId, int valdirty) {
-		
+	public void freePage(PageId pageId, boolean valdirty) {
+		for(int i = 0; i < this.bufferPool.size(); i++) {
+			if(this.bufferPool.get(i).getPageId() == pageId)
+				this.bufferPool.get(i).decrement();	
+			if(valdirty)
+				flushAll();
+		}
 	}
 	
 	/**
@@ -37,7 +58,12 @@ public class BufferManager {
 	 * et remise à 0 des flags/infos/contenus des buffers
 	 */
 	public void flushAll() {
-		
+		for(int i = 0; i < bufferPool.size(); i++) {
+			if (bufferPool.get(i).getDirty()) {
+				DiskManager.writePage(bufferPool.get(i).getPageId(), bufferPool.get(i).getBuff());
+				bufferPool.get(i).initFlags();
+			}	
+		}
 	}
 	
 }
--- Code/src/Frame.java
+++ Code/src/Frame.java
@@ -13,11 +13,101 @@ public class Frame {
 		this.buff = buff;
 	}
 	
+	/**
+	 * incremente pinCount de +1
+	 */
 	public void increment() {
 		this.pinCount++;
 	}
-
+	/**
+	 * decremente pinCount de - 1
+	 */
 	public void decrement() {
 		this.pinCount--;
+	}
+	
+	/**
+	 * Initialise tous les flags à 0 ou null
+	 */
+	public void initFlags() {
+		this.setPageId(null);
+		this.setPinCount(0);
+		this.setDirty(false);
+		this.setBuff(null);
+	}
+	
+	/**
+	 * 
+	 * @return boolean true si tous les flags sont null ou à un (donc initilisé);
+	 */
+	public boolean isNull() {
+		if(this.isDirty() == false && this.pageId == null && this.getPinCount() == 0 && this.getBuff() == null) {
+			return true;
+		}
+		return false;
+	}
+
+	
+	// Getters / Setters
+	/**
+	 * @return the pageId
+	 */
+	public PageId getPageId() {
+		return pageId;
+	}
+
+	/**
+	 * @param pageId the pageId to set
+	 */
+	public void setPageId(PageId pageId) {
+		this.pageId = pageId;
+	}
+	
+
+	/**
+	 * @return the pinCount
+	 */
+	public int getPinCount() {
+		return pinCount;
+	}
+	
+
+	/**
+	 * @param pinCount the pinCount to set
+	 */
+	public void setPinCount(int pinCount) {
+		this.pinCount = pinCount;
+	}
+	
+
+	/**
+	 * @return the dirty
+	 */
+	public boolean isDirty() {
+		return dirty;
+	}
+	
+
+	/**
+	 * @param dirty the dirty to set
+	 */
+	public void setDirty(boolean dirty) {
+		this.dirty = dirty;
+	}
+	
+
+	/**
+	 * @return the buff
+	 */
+	public byte[] getBuff() {
+		return buff;
+	}
+	
+
+	/**
+	 * @param buff the buff to set
+	 */
+	public void setBuff(byte[] buff) {
+		this.buff = buff;
 	}
 }
