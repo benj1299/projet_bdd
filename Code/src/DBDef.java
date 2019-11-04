bc3d9549230c15f7f2d3350efbf4f5f3664f94ab
antonsanaa <50481482+antonsanaa@users.noreply.github.com>
2019-11-04 15:39:20 +0100

    Update DBDef.java

--- Code/src/DBDef.java
+++ Code/src/DBDef.java
@@ -1,6 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Vector;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+
 
 public class DBDef {
+	private static DBDef instance ;
+	 
+	public static DBDef getInstance() {
+      if (instance == null) {
+         synchronized(DBDef.class) {
+            if (instance == null) {
+               instance = new DBDef() ;
+            }
+         }
+      }
+      return instance ;
+   }
 	
 	private Vector<RelDef> tabRelDef;
 	private int count;
@@ -8,13 +28,24 @@ public class DBDef {
 	public DBDef() {
 		this.tabRelDef = new Vector<RelDef>();
 		this.count = 0;
-	}
-	
-	public void init() {
 		
 	}
 	
-	public void finish() {
+	public void init() throws FileNotFoundException, IOException, ClassNotFoundException{
+    	
+    	FileInputStream fis = new FileInputStream(Constants.DB_DIRECTORY +"Catalog.def");
+    	ObjectInputStream ois = new ObjectInputStream(fis);
+    	DBDef.instance = (DBDef) ois.readObject();
+    	ois.close();
+    	
+		
+	}
+	
+	public void finish() throws FileNotFoundException, IOException {
+		FileOutputStream fos = new FileOutputStream(Constants.DB_DIRECTORY +"Catalog.def");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(DBDef.instance);
+		oos.close();
 		
 	}
 	
