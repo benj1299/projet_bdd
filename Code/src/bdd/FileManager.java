84245b7524370bb449bb329f27bd94f1854369ce
antonsanaa <50481482+antonsanaa@users.noreply.github.com>
2019-11-25 13:37:51 +0100

    Update FileManager.java

--- Code/src/bdd/FileManager.java
+++ Code/src/bdd/FileManager.java
@@ -30,6 +30,29 @@ public class FileManager {
 		this.heapFiles = new ArrayList<>();
 	}
 
+	public Rid inserRecordInRelation(Record record, String relName) {
+
+	}
+
+	/**
+	 * Cette méthode doit retourner une liste contenant tous les records de la relation dont le nom est relName.
+	 * @param relName
+	 * @return liste de Record
+	 */
+	public ArrayList<Record> selectAllFromRelation(String relName){
+
+	}
+	/**
+	 * Cette méthode doit retourner une liste contenant tous les records de la relation nomméerelName pour lesquels la valeur sur la colonne idxCol (convertie en chaîne de caractères)est égale à valeur.
+	 * @param String relName
+	 * @param int idxCol, un entier correspondant à un indice de colonne
+	 * @param String  valeur
+	 * @return
+	 */
+	public ArrayList<Record> selectFromRelation(String relName, int idxCol, String valeur ){
+
+	}
+
 
 
 }
