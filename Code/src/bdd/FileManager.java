6eaaa4871b159d05d876b3321da929a711cad299
antonsanaa <50481482+antonsanaa@users.noreply.github.com>
2019-11-25 15:07:57 +0100

    Update FileManager.java

--- Code/src/bdd/FileManager.java
+++ Code/src/bdd/FileManager.java
@@ -75,36 +75,6 @@ public class FileManager {
 
 	}
 	
-	/**
-	 * Pour inserer un record
-	 * @param record
-	 * @return
-	 * @throws Exception 
-	 */
-	public Rid insertRecord(Record record) throws Exception {
-		PageId pid = getFreeDataPageId() ;
-		return this.writeRecordToDataPage(record, pid);	
-	}
-	
-		/**
-	 * 
-	 * @return ArrayList<Record> liste, une liste de Record
-	 */
-	public ArrayList<Record> getAllRecords(){
-		
-		
-		 PageId headerPage = new PageId(relDef.getFileIdx(), 0);
-		  
-		  byte[]  buffheader = bm.getPage(headerPage);	
-		  
-		  List <Record> listeDeRecords = new ArrayList <Record> ();
-		  
-		  for (int i=1; i<=buffheader.lenght; i++){
-		  
-			  listeDeRecords.addAll(buffheader[i].getRecordsInDataPage());
-			  
-		  }
-		
 	
 	
 
